082a0782a87893798f2358fc2de11910d03273d0
Maxwell Paluska <mxpaluska@gmail.com>
2017-06-21 11:59:26 -0400

    initial deltaV() and created empty Makefile

--- rokkit.c
+++ rokkit.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
 #include <math.h>
 
+double deltaV(double exhaustV, double iMass, double dryMass);
+
 int main(void)
 {
-	
+	return 0;
+}
+
+double deltaV(double exhaustV, double iMass, double dryMass)
+{
+	double dV;
+
+	dV = exhaustV * log(iMass/dryMass);
+	return dV;
 }
